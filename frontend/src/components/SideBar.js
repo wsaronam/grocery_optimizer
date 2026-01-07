6c9d4f063acff430da694361dca745a9bc4693ce
Willy Saronamihardja <wsaronam@gmail.com>
2026-01-07 02:54:22 -0800

    debugging
    - trying to debug sidebar grocery list - minor updates

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -5,6 +5,8 @@ import "./styles/SideBar.css";
 
 export default function SideBar({items, onIncrement, onDecrement, onRemove}) {
 
+    console.log(items);
+
     return (
         <div className="sidebar">
             <h1 className="sidebar-title">Your Grocery List</h1>
@@ -15,8 +17,8 @@ export default function SideBar({items, onIncrement, onDecrement, onRemove}) {
                 {items.map((product, quantity) => (
                     <div>
                         <li key={product.barcode} className="sidebar-list-item">
-                            {product.name}
-                            {quantity}
+                            <h1 className="test">{product.name}</h1>
+                            {/* {quantity} */}
                             <div>
                                 <button onClick={() => onIncrement(product.barcode)}>+</button>
                                 <button onClick={() => onDecrement(product.barcode)}>-</button>
